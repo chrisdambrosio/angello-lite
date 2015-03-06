f43448dabdf7112b7b2895bbe9b92fc15d6cf8be
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-03-06 14:45:08 -0800

    set current story

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var myModule = angular.module('Angello', []);
 
 myModule.controller('MainCtrl', function($scope) {
+  $scope.currentStory;
+
+  $scope.setCurrentStory = function(story) {
+    $scope.currentStory = story;
+  };
+
   $scope.stories = [
     { title: 'Story 00', description: 'Description pending.' },
     { title: 'Story 01', description: 'Description pending.' },
@@ -8,5 +14,5 @@ myModule.controller('MainCtrl', function($scope) {
     { title: 'Story 03', description: 'Description pending.' },
     { title: 'Story 04', description: 'Description pending.' },
     { title: 'Story 05', description: 'Description pending.' }
-  ]
+  ];
 });
