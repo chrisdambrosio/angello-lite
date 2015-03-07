dab995950b08ffa1bb16938f484e3d68568071ca
Chris D'Ambrosio <cdambrosio@gmail.com>
2015-03-07 11:16:22 -0800

    story types and statuses

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 var myModule = angular.module('Angello', []);
 
 myModule.controller('MainCtrl', function($scope) {
+  $scope.stories = [
+    { title: 'Story 00', description: 'Description pending.' },
+    { title: 'Story 01', description: 'Description pending.' },
+    { title: 'Story 02', description: 'Description pending.' },
+    { title: 'Story 03', description: 'Description pending.' },
+    { title: 'Story 04', description: 'Description pending.' },
+    { title: 'Story 05', description: 'Description pending.' }
+  ];
+
+  $scope.statuses = [
+    { name: 'Back Log' },
+    { name: 'To Do' },
+    { name: 'In Progress' },
+    { name: 'Code Review' },
+    { name: 'QA Review' },
+    { name: 'Verified' },
+    { name: 'Done' }
+  ];
+
+  $scope.types = [
+    { name: 'Feature' },
+    { name: 'Enhancement' },
+    { name: 'Bug' },
+    { name: 'Spike' }
+  ];
+
+  var buildIndex = function(source, property) {
+    var tempArray = [];
+
+    for (var i = 0, len = source.length; i < len; ++i) {
+      tempArray[source[i][property]] = source[i];
+    }
+    return tempArray;
+  };
+
+  $scope.typesIndex = buildIndex($scope.types, 'name');
+  $scope.statusesIndex = buildIndex($scope.statuses, 'name');
+
+  $scope.setCurrentStatus = function(status) {
+    if (typeof $scope.currentStory !== 'undefined') {
+      $scope.currentStory.status = status.name;
+    }
+  };
+
+  $scope.setCurrentType = function(type) {
+    if (typeof $scope.currentType !== 'undefined') {
+      $scope.currentStory.type = type.name;
+    }
+  };
+
   $scope.createStory = function() {
     $scope.stories.push({
       title: 'New Story',
@@ -12,14 +62,8 @@ myModule.controller('MainCtrl', function($scope) {
 
   $scope.setCurrentStory = function(story) {
     $scope.currentStory = story;
-  };
 
-  $scope.stories = [
-    { title: 'Story 00', description: 'Description pending.' },
-    { title: 'Story 01', description: 'Description pending.' },
-    { title: 'Story 02', description: 'Description pending.' },
-    { title: 'Story 03', description: 'Description pending.' },
-    { title: 'Story 04', description: 'Description pending.' },
-    { title: 'Story 05', description: 'Description pending.' }
-  ];
+    $scope.currentStatus = $scope.statusesIndex[story.status];
+    $scope.currentType = $scope.typesIndex[story.type];
+  };
 });
